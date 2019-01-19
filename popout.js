ff67d60e82dd9c25b839d686dffa2ee3f1770ad7
LefanTan <tan6@ualberta.ca>
2019-01-19 16:37:50 -0700

    Password checker and savign done

--- popout.js
+++ popout.js
@@ -1,1 +1,29 @@
-console.log("Hey");
+chrome.storage.sync.get("pass", function(result){
+    document.getElementById("Intro").innerHTML = result.pass;
+})
+
+document.getElementById("submit").addEventListener("click", Submit);
+
+function Submit(){
+    var userInput = document.getElementById("Password").value;
+ 
+    chrome.storage.sync.get("pass", function(result){
+        if(result.pass == "" || result.pass == null){
+              chrome.storage.sync.set({"pass": userInput}, function() {
+                // Notify that we saved.
+                console.log('password saved');
+              });
+        }else{
+            chrome.storage.sync.get("pass", function(result) {
+               var correctPass = result.pass;
+               if(correctPass != userInput){
+                    alert(userInput + " : " + correctPass);
+                }else{
+                    alert("Correct");
+                }
+            });   
+        }
+    })
+    
+
+}
