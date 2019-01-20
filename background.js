cdf33ddc678c8c13df38e8a6b59e8082fedf239b
LefanTan <tan6@ualberta.ca>
2019-01-19 19:42:55 -0700

    track website spent time

--- background.js
+++ background.js
@@ -3,24 +3,51 @@
 var visitedLinks = {};
 
 chrome.tabs.onUpdated.addListener(TabUpdated);
+chrome.tabs.onActivated.addListener(TabActivated);
+
+var currentTime = 0;
+var timeSpent = 0
+
+function TabActivated(activeInfo){
+
+    if(currentTime == 0){
+        currentTime = GetTime();
+        console.log("CurrentTIme : " + currentTime);
+    }else{
+        timeSpent = GetTimeSpent(currentTime, GetTime());
+        currentTime = 0;
+        console.log("Time spent: " + timeSpent);
+    }
+}
 
 function TabUpdated(tabId,changeInfo,tab){
     var url = tab.url;
     var hostName = GetDomain(url);
 
-    if(hostName in visitedLinks){
-        visitedLinks[hostName] += 3;
-    }else{
-        visitedLinks[hostName] = 1;
-    } 
+    
 
     for (var key in visitedLinks){
-        console.log(key + "'s amount of time visited: " + visitedLinks[key]);
+        //console.log(key + "'s amount of time visited: " + visitedLinks[key]);
     }
+    //console.log(GetTime());
 }
 
 
 function GetDomain(url){
     var hostname = (new URL(url)).hostname;
     return hostname;
+}
+
+function GetTime(){
+    var date = new Date();
+    var hour = date.getHours();
+    var minutes = date.getMinutes();
+    //return current time in minutes
+    return hour * 60 + minutes;
+    //return date.getSeconds();
+}
+
+function GetTimeSpent(previousTime, currentTime){
+    var timeSpent = currentTime - previousTime;
+    return timeSpent;
 }
