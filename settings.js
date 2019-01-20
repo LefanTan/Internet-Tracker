65ad45213e1f21aee480f7f5524fe99c8b02315b
echang49 <46017382+echang49@users.noreply.github.com>
2019-01-20 02:09:55 -0500

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,8 +1,27 @@
-document.getElementById("emailSubmit").addEventListener("click", Submit);
+document.getElementById("emailSubmit").addEventListener("click", UserAddress);
 
 function UserAddress(){
     var userEmail = document.getElementById("email").value;
 	chrome.storage.sync.set({"email": userEmail}, function() {
                 console.log('email updated');
               }); 
+}
+
+document.getElementById("changepass").addEventListener("click", PassChange);
+
+function PassChange(){
+    var userOldInput = document.getElementById("OldPassword").value;
+	var userNewInput = document.getElementById("NewPassword").value;
+	
+    chrome.storage.sync.get("pass", function(result){
+        if(result.pass == userOldInput){
+              chrome.storage.sync.set({"pass": userNewInput}, function() {
+                // Notify that we saved.
+                console.log('password saved');
+				alert("Password Changed");  
+              });
+        }else{
+            alert("Wrong Password, try again");  
+        }
+    })
 }
