3f10eb631e671f1f73577b67dee373f185bf3d94
LefanTan <tan6@ualberta.ca>
2019-01-19 17:25:04 -0700

    updated background scirpt and manifest

--- background.js
+++ background.js
@@ -1,1 +1,15 @@
-var visitedLinks = 
+var visitedLinks = {};
+console.log("tabs[0].url");
+
+var currentUrl = "";
+
+chrome.tabs.onActivated.addListener(NewActive);
+chrome.tabs.onUpdated.addListener(TabUpdated);
+
+function NewActive(tab){
+    console.log(currentUrl);
+}
+
+function TabUpdated(tab){
+    currentUrl = tab.url;
+}
